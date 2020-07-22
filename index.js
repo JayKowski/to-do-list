c947d840ffb3580ebacff8050c41d58ea46bab6a
A L V A R O <alvarop3000@gmail.com>
2020-07-22 16:33:29 -0500

    add event listener for creating new task

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
 // reveal/hide new project form
 // expand/contract project view (hiding the form and the tasks)
 
-const myProjects = [];
+///////////// const myProjects = []; /////////////////
 
 //Should be in it's own module file
 function createNewProjectForm(attatchTo){
@@ -94,15 +94,74 @@ projectForm.addEventListener('submit', function(e){
     const addProject = document.querySelector('.projects');
     const newProject = document.createElement('div');
     newProject.className += 'to-do';
-    let h2 = document.createElement('h2');
+    const h2 = document.createElement('h2');
     h2.className += 'to-do-title';
-    let p = document.createElement('p');
+    const p = document.createElement('p');
     p.className += 'to-do-description';
     h2.innerHTML = `${projectName}`;
     p.innerHTML = `${projectDesc}`;
+    const deleteProject = document.createElement('button');
+    deleteProject.className += 'dlt-prjct';
+    deleteProject.innerHTML = 'Delete Project';
 
     addProject.appendChild(newProject);
     newProject.appendChild(h2);
     newProject.appendChild(p);
-    createNewProjectForm(newProject)
+    createNewProjectForm(newProject);
+    newProject.appendChild(deleteProject);
 });
+
+const taskForm = document.querySelector('.new-task')
+createForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const taskName = taskForm.querySelector('input[name="task-name"]').value
+    const taskDate = taskForm.querySelector('input[name="task-date"]').value
+    const taskPriority = taskForm.querySelector('select[name="priority"]').value
+    console.log(taskPriority)
+    console.log(taskPriority);
+    const addTask = document.querySelector('.to-do');
+    const newTask = document.createElement('div');
+    newTask.className += 'task-list'
+    const span = document.createElement('span');
+    span.className += 'task-text';
+    const p1 = document.createElement('p');
+    p1.className += 'task-desc';
+    p1.innerHTML = `${taskName}`;
+    const p2 = document.createElement('p');
+    p2.className += 'due-date';
+    p2.innerHTML = `${taskDate}`;
+    const div1 = document.createElement('div');
+    div1.className += 'check-form';
+    const divTask1 = document.createElement('div');
+    divTask1.className += 'task-footer';
+    const check1 = document.createElement('i');
+    check1.className += 'far fa-check-circle';
+    const checkbox1 = document.createElement('input');
+    checkbox1.className += 'done-task'
+    checkbox1.setAttribute('type', 'checkbox');
+    const divTask2 = document.createElement('div');
+    divTask2.className += 'task-footer';
+    const check2 = document.createElement('i');
+    check2.className += 'far fa-flag';
+    const spanFooter = document.createElement('span');
+    spanFooter.className += 'task-priority';
+    spanFooter.innerHTML = `${taskPriority}`;
+    const deleteButton = document.createElement('button');
+    deleteButton.className += 'delete-task';
+    const trashBin = document.createElement('i');
+    trashBin.className += 'fas fa-trash-alt';
+
+    addTask.appendChild(newTask);
+    newTask.appendChild(span);
+    span.appendChild(p1);
+    span.appendChild(p2);
+    span.appendChild(div1);
+    div1.appendChild(divTask1);
+    divTask1.appendChild(check1);
+    divTask1.appendChild(checkbox1);
+    div1.appendChild(divTask2);
+    divTask2.appendChild(check2);
+    divTask2.appendChild(spanFooter);
+    newTask.appendChild(deleteButton);
+    deleteButton.appendChild(trashBin);
+})
