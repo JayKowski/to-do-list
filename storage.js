d2bc2852e65c404edc85009cf5660569054e5e34
JayKowski <jaymaina5@gmail.org>
2020-07-24 00:05:23 +0300

    added a localStorage module

---
+++ storage.js
@@ -0,0 +1,42 @@
+export function getProjects(){
+    let myProjects = JSON.parse(localStorage.getItem('myProjects'))
+    return myProjects;
+}
+
+export function addProject(project) {
+    let myProjects;
+    if(localStorage.getItem('myProjects') === null){
+        myProjects = [];
+    } else {
+        myProjects = JSON.parse(localStorage.getItem('myProjects'));
+    }
+    myProjects.push(project);
+    localStorage.setItem('myProjects', JSON.stringify(myProjects));
+    console.log('Project added Successfully', project)
+}
+
+export function updateProject(){
+    if (localStorage.getItem('myProjects')){
+
+    }
+}
+
+export function addTask(task, project){
+    let projects = JSON.parse(localStorage.getItem('myProjects'));
+
+    let index = project.getAttribute('data-index');
+    projects[index].tasks.push(task);
+    localStorage.setItem('myProjects', JSON.stringify(projects))
+    console.log('task added successfully', task)
+
+}
+
+export function deleteProject(key){
+    return localStorage.removeItem(`${key}`)
+}
+
+export function removeTask(key){
+    localStorage.removeItem(`${key}`)
+}
+
+// export { getProjects, addProject, deleteProject, updateProject, removeTask };
